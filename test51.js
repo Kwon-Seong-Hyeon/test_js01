7e256f028360a1a83f6c09f06b1cf5ede2793885
kwon seong hyoen <a3620123@gmail.com>
2023-05-25 22:39:35 +0900

    init

---
+++ test51.js
@@ -0,0 +1,34 @@
+// console.log('tset')
+
+
+// const add1 = (num1, num2, num3 ) =>{
+//     const result = num1+num2+num3;
+// console.log(typeof(num1));
+// console.log('result: ', result);
+// }
+
+
+// add1(144, 115, 240)
+
+const array = [1, 2, 3, 4, 5];
+
+// for (let i in array) {
+//     console.log(i, ':', array[i]);
+// }
+
+// for( let i = 0; i<array.length; i++) {
+//     console.log (i, ':', array[i])
+// }
+// let i = 0;
+// while(i < array.length) {
+//     console.log (i, ':', array[i])
+//     i++;
+// }
+
+let star = '*';
+
+for (let i = 0; i<5; i++){
+    console.log(star)
+}
+
+
